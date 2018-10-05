f5da84c8d5a4c37b53e50a3cff3de36f970ad15b
iainharfield <iainharfield@users.noreply.github.com>
2018-10-05 22:18:57 +0100

    Update vnfUtil.sh

--- vnfUtil.sh
+++ vnfUtil.sh
@@ -9,7 +9,7 @@ TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
 cpu=`top  -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'` 
 ram=$(echo "scale = 2; $CURRENT/$TOTAL*100" | bc)
 hdd=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
-sbHostname=`hostname`
+sbhostName=`hostname`
 privateIP=`hostname -I | awk '{print $1}'`
 
 cpu=`top -bn 1 | awk 'NR>7{s+=$9} END {print s}'`
@@ -20,4 +20,4 @@ cpu=`top -bn 1 | awk 'NR>7{s+=$9} END {print s}'`
 
 
 #echo HOST:$sbHostname, IP:$privateIP, CPU:$cpu, RAM:$ram, HDD:$hdd
-printf "HOST:%s CPU:%s%s RAM:%s" $privateIP $cpu "%" $ram
+printf "HOST:%s IP:%s CPU:%s%s RAM:%s" $sbhostName $privateIP $cpu "%" $ram
